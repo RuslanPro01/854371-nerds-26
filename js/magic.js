841650ef0273f36f5d8104dd5748ebedd4db42fb
RuslanPro01 <45145160+RuslanPro01@users.noreply.github.com>
2019-11-02 12:50:32 +0300

    Последние доработки

--- js/magic.js
+++ js/magic.js
@@ -12,17 +12,17 @@ button.addEventListener("click", function (evt) {
     modalPopUp.classList.add("popUpShow");
     modalPopUp.classList.add("popupOpenAnimate");
     setTimeout(() => {
-        modalPopUp.classList.remove("popupOpenAnimate");
+      modalPopUp.classList.remove("popupOpenAnimate");
     }, 2000);
     fullName.focus();
 });
 
 closePopUp.addEventListener("click", closePopUpFnc);
 
-window.addEventListener("keydown", function (evt) {
-    if (evt.keyCode === 27) {
+window.addEventListener("keydown", function (e) {
+    if (e.keyCode === 27) {
         if (modalPopUp.classList.contains("popUpShow")) {
-            closePopUpFnc(evt);
+            closePopUpFnc(e);
         }
     }
 });
@@ -33,9 +33,9 @@ function closePopUpFnc(evt) {
     modalPopUp.classList.add("popupOpenAnimate");
 
     setTimeout(() => {
-        modalPopUp.classList.remove("popUpShow");
-        modalPopUp.classList.remove("popupOpenAnimate");
-        modalPopUp.classList.remove("popupError");
+      modalPopUp.classList.remove("popUpShow");
+      modalPopUp.classList.remove("popupOpenAnimate");     
+      modalPopUp.classList.remove("popupClose");
     }, 800);
 }
 
@@ -46,4 +46,7 @@ modalPopUp.addEventListener("submit", function (evt) {
         modalPopUp.offsetWidth = modalPopUp.offsetWidth;
         modalPopUp.classList.add("popupError");
     }
+    setTimeout(() => {
+        modalPopUp.classList.remove("popupError");
+    }, 600);
 })
